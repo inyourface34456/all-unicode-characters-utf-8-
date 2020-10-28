d27968293c15f3c0462b45274f3adfd71860f215
inyourface3445 <trenten_miller@etownstudents.org>
2020-10-28 16:44:01 +0000

    everything

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-for i in range(0x0000, 0xffff):
-    print (chr(i))
+counter = 0
+for i in range(0x00000, 0xd800):
+    print("\033[1;32;40m Bright Green  \n")
+    print(chr(i))
+    h = hex(i)
+    print(h)
+    counter = counter + 1
+    print('this is the number of charaters that i printed so far:')
+    print(counter)
+hello = print ('mê')
