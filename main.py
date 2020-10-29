ab73b70165a0d103d0874f597b6d7317c516b5ea
inyourface3445 <trenten_miller@etownstudents.org>
2020-10-29 15:37:32 +0000

    i added 0xe800 to 0xfffff and color changes

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 counter = 0
-for i in range(0x00000, 0xd800):
-    print("\033[1;32;40m Bright Green  \n")
-    print(chr(i))
+for i in range(0x00000, 0xd7ff):
+    print("\033[1;32;40m \n")
+    print(chr(i)) 
     h = hex(i)
     print(h)
     counter = counter + 1
     print('this is the number of charaters that i printed so far:')
     print(counter)
-hello = print ('mê')
+
+for i in range(0xe8000, 0xfffff):
+  print("\033[1;35;40m \n")
+  print(h)
+  counter = counter + 1
+  print('this is the number of charaters that i printed so far:')
+  print(counter)
+
+print(chr(0xfffff))
+print(h)
+print('this is the number of charaters that i printed so far:')
+counter = counter + 1
+print(counter)
