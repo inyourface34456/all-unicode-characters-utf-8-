56d8751b0b5e765eaa8a673c97b6c2edc73d43e4
inyourface3445 <trenten_miller@etownstudents.org>
2020-10-29 15:46:52 +0000

    added comments

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
-counter = 0
-for i in range(0x00000, 0xd7ff):
-    print("\033[1;32;40m \n")
-    print(chr(i)) 
-    h = hex(i)
-    print(h)
-    counter = counter + 1
-    print('this is the number of charaters that i printed so far:')
-    print(counter)
+counter = 0 # set the counter to zero
+for i in range(0x00000, 0xd7ff): # covers the first half of the chars
+    print("\033[1;32;40m \n") # adds color
+    print(chr(i)) # prints the unicode char
+    h = hex(i) # grabs the hex value (for convince)
+    print(h) # prints the hex value
+    counter = counter + 1 # how many chars that the program printed
+    print('this is the number of charaters that i printed so far:') # some text
+    print(counter) # prints the counter
 
-for i in range(0xe8000, 0xfffff):
-  print("\033[1;35;40m \n")
-  print(h)
-  counter = counter + 1
-  print('this is the number of charaters that i printed so far:')
-  print(counter)
+for i in range(0xe8000, 0xfffff): # covers the chars after high suoragates
+  print("\033[1;35;40m \n") # changes the colors (so i know that it works)
+  print(h) # prints the unicode char
+  counter = counter + 1 # how many chars that the program printed
+  print('this is the number of charaters that i printed so far:') # some text
+  print(counter) # prints the counter
 
-print(chr(0xfffff))
-print(h)
-print('this is the number of charaters that i printed so far:')
-counter = counter + 1
-print(counter)
+print(chr(0xfffff)) # prints the final char
+print(h) # prints the hex value
+print('this is the number of charaters that i printed so far:') # some text
+counter = counter + 1 # how many chars that the program printed
+print(counter) # prints the counter
