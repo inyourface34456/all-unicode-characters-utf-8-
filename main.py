06c6d0d43f7618a7ee179d9cd26f61a1f68c34cc
inyourface3445 <trenten_miller@etownstudents.org>
2020-11-11 14:10:37 +0000

    everything

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
-counter = 0 # set the counter to zero
-for i in range(0x00000, 0xd800): # covers the first half of the chars
-  print("\033[1;32;40m \n") # adds color
-  print(chr(i)) # prints the unicode char
-  h = hex(i) # grabs the hex value (for convince)
-  print(h) # prints the hex value
-  counter = counter + 1 # how many chars that the program printed
-  print('this is the number of charaters that i printed so far:') # some text
-  print(counter) # prints the counter
-
-
-for j in range(0xe8000, 0xfffff): # covers the chars after high suoragates
-  print("\033[1;35;40m \n") # changes the colors (so i know that it works)
-  print(chr(j))
-  # print(j) for debugging
-  h = hex(j)
-  print(h) # prints the unicode char
-  counter = counter + 1 # how many chars that the program printed
-  print('this is the number of charaters that i printed so far:') # some text
-  print(counter) # prints the counter
-
-print(chr(0xfffff)) # prints the final char
-print(h) # prints the hex value
-print('this is the number of charaters that i printed so far:') # some text
-counter = counter + 1 # how many chars that the program printed
-print(counter) # prints the counter
+def unicode():
+  global x
+  while(x != y):
+    for i in range(0x0, 0xd800):
+        print(chr(i))
+        f = open(logging, "a")
+        f.write(chr(i))
+        f.close()
+    for i in range(0xe000, 0xfffff):
+      j = chr(i)
+      print(j)
+      f = open(logging, "a")
+      f.write(j)
+      f.close()
+    x = x + 1
+    print(x, 'repations done')
+y = int(input('enter the number of reps that this should do \n (each repatation is about 3.65 MB) '))
+file = str(input('what file do you want to use for logging? '))
+MB = y*3.65
+promt = 'are you shure that you want to print', MB, 'MB of charaters (y or n) '
+r_u_shure = str(input(promt))
+x = 0 
+logging_file = file, '.txt'
+logging = str(logging_file)
+if (r_u_shure == y):
+  unicode()
